fc3f1d0728e1482780af9a35cd52ec4df668b64c
Ramkumar G <ramkumar.g@telliant.net>
2022-01-12 19:14:31 +0530

    first commit

---
+++ routes/admin.js
@@ -0,0 +1,87 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models/sql')
+var geoip = require('geoip-lite');
+var md5 = require('md5');
+
+router.get('/', function (req, res) {
+     try {
+        db.query("SELECT * FROM USER ", (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+                res.send(result)
+            } else {
+                res.send(JSON.stringify({message: 'no user found'}))
+            }
+        })
+    } catch (e) {
+        res.send(e)
+    }
+})
+
+router.get('/:name', function (req, res) {
+    var username = req.params.name
+     try {
+
+        db.query("SELECT NAME, EMAIL, LOCATION FROM USER WHERE USERNAME = ?", [username], (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+                res.send(result)
+            } else {
+                res.send(JSON.stringify({message: 'user not found'}))
+            }
+        })
+
+
+    } catch (e) {
+        res.send(e)
+    }
+})
+
+
+router.post('/update', (req, res)=>{
+    var name = req.body.name;
+    var password = md5(req.body.password);
+    var location = req.body.location;
+    var username =  req.body.username;
+     try {
+        db.query("UPDATE USER SET NAME=?, PASSWORD=?, LOCATION=? WHERE USERNAME = ?", [name,  password, location, username], (err, result) => {
+            if (err) throw err;
+            res.send({ message: "User updated successfully" })
+        })
+     } catch (e) {
+         res.send(e)
+     }
+})
+
+router.post("/add", (req, res) => {
+    var name = req.body.name;
+   // var password = md5(req.body.password);
+   var password = md5('Change@123');
+    var email = req.body.email;
+    var role = req.body.role;
+    var ip = req.ip;
+    var location = geoip.lookup(ip)? geoip.lookup(ip) : "" ;
+    var username = req.body.email ? req.body.email.split('@')[0] : ''
+    try {
+
+        db.query("SELECT USERNAME FROM USER WHERE USERNAME = ?", [username], (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+                res.send({ "message": "User Already exists" })
+            } else {
+                db.query("INSERT INTO USER (NAME, USERNAME, PASSWORD, EMAIL, LOCATION, ROLE) VALUES (?, ?, ?, ?, ?, ?)", [name, username, password, email, location, role], (err, result) => {
+                    if (err) throw err;
+                    res.send({ "message": "User created successfully" })
+                })
+
+            }
+        })
+
+
+    } catch (e) {
+        res.send(e)
+    }
+
+})
+module.exports = router;
---
+++ routes/user.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var jwt = require('jsonwebtoken');
+var db = require('../models/sql');
+var md5 = require('md5');
+
+router.get('/', function (req, res) {
+    var decoded = jwt.decode(req.cookies.token, process.env.SECRET_KEY)
+    var username = decoded?.username?decoded.username:''
+     try {
+
+        db.query("SELECT NAME, EMAIL, LOCATION FROM USER WHERE USERNAME = ?", [username], (err, result) => {
+            if (err) throw err;
+            if (result.length > 0) {
+                res.send(result)
+            } else {
+                res.send(JSON.stringify({message: 'user not found'}))
+            }
+        })
+
+
+    } catch (e) {
+        res.send(e)
+    }
+})
+
+
+router.post('/update', (req, res)=>{
+    var name = req.body.name;
+    var password = md5(req.body.password);
+    var location = req.body.location;
+    var decoded = jwt.decode(req.cookies.token, process.env.SECRET_KEY)
+    var username = decoded?.username?decoded.username:''
+     try {
+        db.query("UPDATE USER SET NAME=?, PASSWORD=?, LOCATION=? WHERE USERNAME = ?", [name,  password, location, username], (err, result) => {
+            if (err) throw err;
+            res.send({ message: "User updated successfully" })
+        })
+     } catch (e) {
+         res.send(e)
+     }
+})
+module.exports = router;
